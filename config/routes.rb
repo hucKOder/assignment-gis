931107cd58d160b103ee8ab0c20d4a4cd1d8e113
hucKoder <hucko.andrej.mail@gmail.com>
2018-12-04 03:16:12 +0100

    tectonic plates + search form for both views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => redirect('/map/show')
   get 'map/show'
   get 'map/search'
   get 'map/vulcanoes_near_route'
   get 'plates/get_plate'
-  get 'plates/show'
+  post 'plates/show'
 end
